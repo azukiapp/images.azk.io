04542f6c1754cce305e743a77243f3f8315da0fa
Marcus Gadbem <mcsgad@gmail.com>
2015-09-28 00:53:10 -0300

    Removed old analytics

--- src/assets/js/app.boot.js
+++ src/assets/js/app.boot.js
@@ -5,17 +5,6 @@ import _            from 'lodash';
 import AppRoutes from './app.router.js'
 
 /**
- * Analytics Settings
- */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-41383022-2']);
-_gaq.push(['_setDomainName', 'azk.io']);
-
-function push_analytics(label) {
-  _gaq.push(['_trackPageview', label]);
-}
-
-/**
  * Boot app
  */
 React.render(
